aab933c37ef7c5619e8b0d2667fd4ceae70ab562
Brian Delhaisse <bdelhais@ulb.ac.be>
2017-11-06 10:57:40 +0100

    update setup.py to account for MacOSX systems

--- cython/setup.py
+++ cython/setup.py
@@ -6,14 +6,21 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
+from sys import platform
 
 sourcefiles = ['pykondo.pyx',
                # '../src/rcb4.cpp',
                '../src/kondo.cpp'] #'ckondo.pxd', '../src/kondo.cpp']
 compile_opts = ['-std=c++11']
 include_dirs = ['../include/', #rcb4.h, kondo.h
-                '/usr/include/'] # ftdi.h
-library_dirs = ['/usr/lib/x86_64-linux-gnu/'] # libftdi.so
+                '/usr/include/', # ftdi.h
+                '/usr/local/include/'] # ftdi.h
+if platform=='linux' or platform=='linux2': # Linux
+    library_dirs = ['/usr/lib/x86_64-linux-gnu/'] # libftdi.so
+elif platform=='darwin': # MacOSX
+    library_dirs = ['/usr/local/lib/']
+else:
+    raise ValueError("This setup only works on a Linux system or MacOSX.")
 libraries = ['ftdi']
 
 ext_modules = [Extension('pykondo',
